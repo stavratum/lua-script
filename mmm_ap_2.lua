be76c735e3acc10b1bb8f201b68da66839033fc8
stavratum <89344034+stavratum@users.noreply.github.com>
2022-05-16 14:09:10 +0500

    Update mmm_ap_2.lua

--- mmm_ap_2.lua
+++ mmm_ap_2.lua
@@ -99,7 +99,7 @@ local Initialize = function(Side)
                 game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
                 ln:Destroy() 
             end)
-        elseif ScrollType(Side)=="Upscroll"then 
+        else
             v.ChildAdded:Connect(function(ln)
                 repeat task.wait() until ln.AbsolutePosition.Y+75 <= Y
                 game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
