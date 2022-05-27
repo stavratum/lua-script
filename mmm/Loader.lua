b6921664c5c14895ccfb3fe8418fac1b3b469807
stavratum <89344034+stavratum@users.noreply.github.com>
2022-05-27 23:40:57 +0500

    Update Loader.lua

--- mmm/Loader.lua
+++ mmm/Loader.lua
@@ -4,12 +4,12 @@ local AP_VERSION = "v1"
 --// Config your keybinds (table)
 
 Controls = {
-    [4] = {Left = "D", Down = "F", Up = "J", Right = "K"},
-    [5] = {Left = "D", Down = "F", Up = "J", Right = "K", Space = "Space"},
-    [6] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L"},
-    [7] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L", Space = "Space"},
-    [8] = {Left = "A", Down = "S", Up = "D", Right = "F", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"},
-    [9] = {Left = "A", Down = "S", Up = "D", Right = "F", Space = "Space", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"}
+    [4] = {Left = "D", Down = "F", Up = "J", Right = "K"}, --//4K
+    [5] = {Left = "D", Down = "F", Up = "J", Right = "K", Space = "Space"}, --//5K
+    [6] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L"}, --//6K
+    [7] = {Left = "S", Up = "D", Right = "F", Left2 = "J", Down = "K", Right2 = "L", Space = "Space"}, --//7K
+    [8] = {Left = "A", Down = "S", Up = "D", Right = "F", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"}, --//8K
+    [9] = {Left = "A", Down = "S", Up = "D", Right = "F", Space = "Space", Left2 = "J", Down2 = "K", Up2 = "L", Right2 = "Semicolon"} --//9K
 }
 
 -- Script loader
