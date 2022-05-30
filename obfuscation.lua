3e9fcdda9d22bee03bdde3b7e2a0dbeae67428b2
stavratum <89344034+stavratum@users.noreply.github.com>
2022-05-30 23:13:56 +0500

    Create obfuscation.lua

---
+++ obfuscation.lua
@@ -0,0 +1,98 @@
+--obfuscation test
+
+do 
+local a__,b_,c___ = loadstring,print,string.char
+a__,c___,b_=b_,c___,a__;
+a_,b__,c___=c___,b_,a__;
+c_,b__,c___=c_,b__,c___
+c__,b__,a___=b_,c___,a__;
+b_,a__,c___=c__,b_,a__;
+
+local loadstring,print = a__,c_
+
+local I_HATE_NIGGERS = {
+    ['IlIIlIl'..'IllIlIlII'] = function(value)
+        return value -  - -   -  -    -    -  - - 2
+    end;
+    ['Il'..'IllIlIIlIlIlII'] = function(your_mom)
+        your_mom[weight] = your_mom[weight] - 1000
+        if your_mom[weight] >= 1000 then 
+            return (2166 - - - - - - - - - - - 1 + 3 + 5 + 0 -  - - - - 1 - - -  - 7)
+        end
+        return 'kys dumb furry'
+    end
+}
+({
+    [true]=function(IlIIlIlIIlIlIlII)
+        getgenv()[tostring(
+            
+    (function()
+        local ab = 78+0xd
+        ab = IlIIlIlIIlIlIlII[ab]()
+        ddddd = 1
+        repeat ddddd = ddddd+1 ab = ab -   - -  -  -  -  - -  -    -  - string.char(49) if ddddd>500 then break end until true and
+        I_HATE_NIGGERS['IlIIlIlIllIlIlII'](ab)==IlIIlIlIIlIlIlII[tonumber((string.char(57)..string.char(50)))]()[tonumber(tostring(
+1/2+1/8+1/8+1/8+1/8+0x22706a+0x42d/2+0x42d/2+0x75d/32*2*2*2*2-     - -   -  - 0.5/6):gsub('3','')..'0')/1000]
+ddddd = math.random(string.char(49),#('qwertyuiopasdfghjkl_'))
+return I_HATE_NIGGERS['IlIIlIlIllIlIlII'](ab)
+end)())] = (function(__________________________,___________________________________,______________________________,___________________________)
+local ____________________________ = ''
+for i,v in pairs(__________________________:split'/')do 
+    ____________________________ = ____________________________..string.char(v)
+end
+return ____________________________
+end)("109/111/116/104/101/114/32/108/111/118/101/32/109/101/32/111/110/108/121/32/111/110/101/32/116/105/109/101/32/111/110/101/32/116/105/109/101")
+    end;
+    [false]=function()print'false'end;
+})[mega_sex_dupe_______________________________ or
+ not    not not  not not  not   not
+  not    not       not
+ not   not      not  not
+   not  not    not not not
+   not true
+and not not not not true and not nil and 'table'==type(
+{"who?";
+    'cares';
+    "what";
+    'ever';
+    "when";
+    'did I ask'}
+)](
+(
+    {
+    [90]=function(amazing_wholesome_cute_shock_kitty_pink_room)
+        return 
+        tonumber(tostring(
+            0+0
+            + amazing_wholesome_cute_shock_kitty_pink_room/4
+            + amazing_wholesome_cute_shock_kitty_pink_room/8 
+            + amazing_wholesome_cute_shock_kitty_pink_room/8 
+            + amazing_wholesome_cute_shock_kitty_pink_room/2)
+            )+tonumber(tostring(0x22706a/2/50):gsub('570.02',''))
+    end;
+    [91]=function(mat_ebal_koncheniy_syn_schluhi)
+        return (
+            {
+                [1/2+1/8+1/8+1/8+1/8+0x22706a] = 1875;
+                [0x42d/2+0x42d/2+0x75d/32*2*2*2*2] = 2954
+            }
+        )[2257003/2+2257003/8+2257003/8+2257003/8+2257003/16*2]
+    end;
+    [92]=function()
+        return
+            {
+                [76.502]=function()
+                    return 216/2/4*2*2 - - 0; 
+                end
+            }
+    end
+    }
+)
+)
+end
+
+for __________________________________________________________,_______________________________________________________ in pairs(getgenv())do
+    if type(_______________________________________________________)=='string'then 
+        print(_______________________________________________________)
+    end
+end
