bd986b8808f0c028f52402c776c6742e1bcab34b
stavratum <89344034+stavratum@users.noreply.github.com>
2022-06-05 01:31:15 +0500

    Update Blacklisted.lua

--- mmm/Blacklisted.lua
+++ mmm/Blacklisted.lua
@@ -1,6 +1,3 @@
 return {
-  [2595782294] = "Retarded skid";
-  [3574298146] = "Retarded skid";
-  [3574447469] = "Retarded skid";
-  [3389950108] = "How's skidding man?"
+  [1470238059] = "73.207.176.114";
 }
