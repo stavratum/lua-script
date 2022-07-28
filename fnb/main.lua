d3c9849c6b495b6b1d933c0e38c57d50713cdade
stavratum <89344034+stavratum@users.noreply.github.com>
2022-07-28 23:53:46 +0500

    Update main.lua

--- fnb/main.lua
+++ fnb/main.lua
@@ -2,9 +2,9 @@ setclipboard"https://discord.gg/tVWz96nUu4"
 
 game:GetService"StarterGui":SetCore("SendNotification",
     { 
-        Title = "Get the new script at";
-        Text = "https://discord.gg/tVWz96nUu4";
-        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"
+        Title = "Get updated version";
+        Text = "The invite is in your clipboard";
+        Duration = 5;
     }
 )
 
