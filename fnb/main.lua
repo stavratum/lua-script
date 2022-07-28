52d8700b065e39e6b9657ab2e78ccc49e211ed92
stavratum <89344034+stavratum@users.noreply.github.com>
2022-07-28 23:15:07 +0500

    diamond dupe redeem twitter anti lag issue 90

--- fnb/main.lua
+++ fnb/main.lua
@@ -1,129 +1,124 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
-
-local uwuware = loadstring(game:HttpGet'https://raw.githubusercontent.com/stavratum/lua-script/main/fnb/uwuware_edit.lua')()
-local Maid = loadstring(game:HttpGet'https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Maid.lua')().new()
-
-local Client = game:GetService'Players'.LocalPlayer
-local VirtualInputManager = game:GetService'VirtualInputManager'
-local RunService = game:GetService'RunService'
-local ReplicatedStorage = game:GetService'ReplicatedStorage'
-
-local ChildAdded
-
-local function IsOnHit(_)
-    return _ and require(_).Type == 'OnHit' or false
-end
-
-local function FindDescendant(Inst,Excepted)
-    for i,v in pairs(Inst:GetDescendants()) do
-        if tostring(v) == Excepted then
-            return v
-        end
-    end
-    return nil
-end
-
-local Window = uwuware:CreateWindow"Friday Night Bloxxin'"
-Window:AddSlider{text="Offset (ms)",flag = "ms",min = -75, max = 75,value = 0}
-Window:AddToggle{text="Toggle Autoplayer",flag = "yes",state = true}
-Window:AddButton{text="Instant Solo",callback=function()Client.PlayerGui:WaitForChild'SingleplayerUI'.ButtonPressed:FireServer()end}
-
-Window:AddButton{text="Unload Script",callback=function()
-    ChildAdded:Disconnect()
-    Maid:DoCleaning()
-        
-    uwuware.base:Destroy()
-    script:Destroy()
-end}
-Window:AddBind{text = "Close GUI", key = Enum.KeyCode.Delete, callback = function()uwuware:Close()end}
-uwuware:Init()
-
-local Init = function(Child)
-    wait(1)
-    repeat wait() until Child.Config.TimePast.Value >= -1
-    
-    local Arrows = Child.Game[Child.PlayerSide.Value].Arrows
-    local IncomingNotes = Arrows.IncomingNotes:children()
-    
-    local Song = FindDescendant(ReplicatedStorage.Songs,Child.LowerContainer.Credit.Text:split'\n'[1]:split' ('[1])
-    local GimmickNotes
-    print('Song: ' .. tostring(Song))
-    if Song then
-        GimmickNotes = Song:FindFirstChild'MultiplieGimmickNotes' and Song:FindFirstChild'MultiplieGimmickNotes'.Value == 'OnHit'  or 
-        Song:FindFirstChildOfClass'ModuleScript' and Song:FindFirstChildOfClass'ModuleScript':FindFirstChild'GimmickNotes'
-        or Song:FindFirstChild'GimmickNotes'
-    end
-    GimmickNotes = GimmickNotes and GimmickNotes.Value == 'OnHit' or nil
-    
-    local Keybinds,KeyCode = Client.Input.Keybinds,Enum.KeyCode
-    local Keys = {
-        [4] = {
-            Left = KeyCode[Keybinds.Left.Value],
-            Down = KeyCode[Keybinds.Down.Value],
-            Up = KeyCode[Keybinds.Up.Value],
-            Right = KeyCode[Keybinds.Right.Value]
-        },
-        [6] = {
-            S = KeyCode[Keybinds.L3.Value],
-            D = KeyCode[Keybinds.L2.Value],
-            F = KeyCode[Keybinds.L1.Value],
-            J = KeyCode[Keybinds.R1.Value],
-            K = KeyCode[Keybinds.R2.Value],
-            L = KeyCode[Keybinds.R3.Value]
-        },
-        [8] = {
-            A = KeyCode[Keybinds.L4.Value],
-            S = KeyCode[Keybinds.L3.Value],
-            D = KeyCode[Keybinds.L2.Value],
-            F = KeyCode[Keybinds.L1.Value],
-            H = KeyCode[Keybinds.R1.Value],
-            J = KeyCode[Keybinds.R2.Value],
-            K = KeyCode[Keybinds.R3.Value],
-            L = KeyCode[Keybinds.R4.Value ~= ';' and Keybinds.R4.Value or 'Semicolon'] -- I am so amazing at hardcode
-        }
-    }
-    Keys = Keys[#IncomingNotes] or Keys[#IncomingNotes - 1]
-    Keys.Space = KeyCode[Keybinds.Space.Value]
-    
-    Keybinds,KeyCode = nil
-    
-    for _,Holder in pairs(IncomingNotes) do
-        Maid:GiveTask(
-            Holder.ChildAdded:Connect(
-                function(Arrow)
-                    if Arrow.HellNote.Value
-                    or IsOnHit(Arrow:FindFirstChildOfClass'ModuleScript')
-                    and GimmickNotes then return end
-                    
-                    local Input = Keys[Holder.name]
-                    task.wait(.4 + math.floor(uwuware.flags.ms)/1000) --like this for now im lazy
-                    
-                    if not uwuware.flags.yes then return end
-                        
-                    VirtualInputManager:SendKeyEvent(true,Input,false,nil)
-                    repeat task.wait() until not Arrow or not Arrow:FindFirstChild'Frame' or Arrow.Frame.Bar.Size.Y.Scale <= .35
-                    VirtualInputManager:SendKeyEvent(false,Input,false,nil)
-                end
-            )
-        )
-    end
-    Child.Destroying:Wait()
-    Maid:DoCleaning()
-end
-
-ChildAdded = Client.PlayerGui.ChildAdded:Connect(
-    function(Child)
-        if Child.name ~= 'FNFEngine' then return end 
-        Init(Child)
-    end
-)
-
-for i,v in pairs(game:GetService"Workspace":GetDescendants()) do
-    if v:IsA'ProximityPrompt' then
-        v.HoldDuration = 0
-    end
-end
-
-if Client.PlayerGui:FindFirstChild'FNFEngine' then
-    Init(Client.PlayerGui.FNFEngine)
-end
+local a = game --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+if not a:IsLoaded() then --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+    a.Loaded:Wait() --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+end --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local b = loadstring(a:HttpGet "https://raw.githubusercontent.com/stavratum/lua-script/main/fnb/uwuware_edit.lua")() --[[ hi  --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local c = --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+    loadstring( --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+    a:HttpGet "https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Maid.lua" --[[ hi ]]
+)().new() --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local d = a:GetService "Players".LocalPlayer --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local e = a:GetService "VirtualInputManager" --[[ hi ]]
+local f = a:GetService "RunService" --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local g = a:GetService "ReplicatedStorage" --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local h --[[ hi  --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+local function i(j) --[[ hi ]]
+    return j and require(j).Type == "OnHit" or false --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+end --[[ hi ]]
+local function k(l, m) --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+    for n, o in pairs(l:GetDescendants()) do --[[ hi ]]
+        if tostring(o) == m then --[[ hi ]]
+            return o --[[ hi ]]
+        end --[[ hi ]]
+    end --[[ hi ]]
+    return nil --[[ hi ]]
+end --[[ hi ]]
+local p = b:CreateWindow "Friday Night Bloxxin'" --[[ hi ]]
+p:AddSlider {text = "Offset (ms)", flag = "ms", min = -75, max = 75, value = 0} --[[ hi ]]
+p:AddToggle {text = "Toggle Autoplayer", flag = "yes", state = true} --[[ hi ]]
+p:AddButton {text = "Instant Solo", callback = function() --[[ hi ]]
+        d.PlayerGui:WaitForChild "SingleplayerUI".ButtonPressed:FireServer() --[[ hi ]]
+    end} --[[ hi ]]
+p:AddButton {text = "Unload Script", callback = function() --[[ hi ]]
+        h:Disconnect() --[[ hi ]]
+        c:DoCleaning() --[[ hi ]]
+        b.base:Destroy() --[[ hi ]]
+        script:Destroy() --[[ hi ]]
+    end} --[[ hi ]]
+p:AddBind {text = "Close GUI", key = Enum.KeyCode.Delete, callback = function() --[[ hi ]]
+        b:Close() --[[ hi ]]
+    end} --[[ hi ]]
+b:Init() --[[ hi ]]
+local q = function(r) --[[ hi ]]
+    wait(1) --[[ hi ]]
+    repeat --[[ hi ]]
+        wait() --[[ hi ]]
+    until r.Config.TimePast.Value >= -1 --[[ hi ]]
+    local s = r.Game[r.PlayerSide.Value].Arrows --[[ hi ]]
+    local t = s.IncomingNotes:children() --[[ hi ]]
+    local u = k(g.Songs, r.LowerContainer.Credit.Text:split "\n"[1]:split " ("[1]) --[[ hi ]]
+    local v --[[ hi ]]
+    if u then --[[ hi ]]
+        v = --[[ hi ]]
+            u:FindFirstChild "MultiplieGimmickNotes" and u:FindFirstChild "MultiplieGimmickNotes".Value == "OnHit" or --[[ hi ]]
+            u:FindFirstChildOfClass "ModuleScript" and --[[ hi ]]
+                u:FindFirstChildOfClass "ModuleScript":FindFirstChild "GimmickNotes" or --[[ hi ]]
+            u:FindFirstChild "GimmickNotes" --[[ hi ]]
+    end --[[ hi ]]
+    v = v and v.Value == "OnHit" or nil --[[ hi ]]
+    local w, x = d.Input.Keybinds, Enum.KeyCode --[[ hi ]]
+    local y = { --[[ hi ]]
+        [4] = {Left = x[w.Left.Value], Down = x[w.Down.Value], Up = x[w.Up.Value], Right = x[w.Right.Value]}, --[[ hi ]]
+        [6] = { --[[ hi ]]
+            S = x[w.L3.Value], --[[ hi ]]
+            D = x[w.L2.Value], --[[ hi ]]
+            F = x[w.L1.Value], --[[ hi ]]
+            J = x[w.R1.Value], --[[ hi ]]
+            K = x[w.R2.Value], --[[ hi ]]
+            L = x[w.R3.Value] --[[ hi ]]
+        }, --[[ hi ]]
+        [8] = { --[[ hi ]]
+            A = x[w.L4.Value], --[[ hi ]]
+            S = x[w.L3.Value], --[[ hi ]]
+            D = x[w.L2.Value], --[[ hi ]]
+            F = x[w.L1.Value], --[[ hi ]]
+            H = x[w.R1.Value], --[[ hi ]]
+            J = x[w.R2.Value], --[[ hi ]]
+            K = x[w.R3.Value], --[[ hi ]]
+            L = x[w.R4.Value ~= ";" and w.R4.Value or "Semicolon"] --[[ hi ]]
+        } --[[ hi ]]
+    } --[[ hi ]]
+    y = y[#t] or y[#t - 1] --[[ hi ]]
+    y.Space = x[w.Space.Value] --[[ hi ]]
+    w, x = nil --[[ hi ]]
+    for j, z in pairs(t) do --[[ hi ]]
+        c:GiveTask( --[[ hi ]]
+            z.ChildAdded:Connect( --[[ hi ]]
+                function(A) --[[ hi ]]
+                    if A.HellNote.Value or i(A:FindFirstChildOfClass "ModuleScript") and v then --[[ hi ]]
+                        return --[[ hi ]]
+                    end --[[ hi ]]
+                    local B = y[z.name] --[[ hi ]]
+                    task.wait(.4 + math.floor(b.flags.ms) / 1000) --[[ hi ]]
+                    if not b.flags.yes then --[[ hi ]]
+                        return --[[ hi ]]
+                    end --[[ hi ]]
+                    e:SendKeyEvent(true, B, false, nil) --[[ hi ]]
+                    repeat --[[ hi ]]
+                        task.wait() --[[ hi ]]
+                    until not A or not A:FindFirstChild "Frame" or A.Frame.Bar.Size.Y.Scale <= .35 --[[ hi ]]
+                    e:SendKeyEvent(false, B, false, nil) --[[ hi ]]
+                end --[[ hi ]]
+            ) --[[ hi ]]
+        ) --[[ hi ]]
+    end --[[ hi ]]
+    r.Destroying:Wait() --[[ hi ]]
+    c:DoCleaning() --[[ hi ]]
+end --[[ hi ]]
+h = --[[ hi ]]
+    d.PlayerGui.ChildAdded:Connect( --[[ hi ]]
+    function(r) --[[ hi ]] --[[ hi ]]
+        if r.name ~= "FNFEngine" then --[[ hi ]]
+            return --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+        end --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+        q(r) --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]] --[[ hi ]]
+    end --[[ hi ]]
+) --[[ hi ]]
+for n, o in pairs(game:GetService "Workspace":GetDescendants()) do --[[ hi ]]
+    if o:IsA "ProximityPrompt" then --[[ hi ]]
+        o.HoldDuration = 0 --[[ hi ]]
+    end --[[ hi ]]
+end --[[ hi ]]
+if d.PlayerGui:FindFirstChild "FNFEngine" then --[[ hi ]]
+    q(d.PlayerGui.FNFEngine) --[[ hi ]]
+end --[[ hi ]]
